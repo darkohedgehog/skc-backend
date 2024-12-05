ca820d7e3b65bb1db33fa46849c7719be703d3f7
Darko Živić <zivic.darko79@gmail.com>
2024-12-05 11:33:39 +0100

    Added new files

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
         config: {
             provider: 'local',
             providerOptions: {
-                sizeLimit: 1000000, // Maksimalna veličina fajla u bajtovima
+                sizeLimit: 100000000, // Maksimalna veličina fajla u bajtovima
             },
             actionOptions: {
                 upload: {},
